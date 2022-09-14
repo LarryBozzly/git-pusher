114641933918ebd8bd53af012c0c1819e1b8f722
larrybozzly <larrybozzly@hotmail.com>
2022-09-14 18:46:57 +0300

    test

---
+++ test.js
@@ -0,0 +1,30 @@
+fs = require('fs');
+const { exec } = require('child_process');
+
+
+let git_item_name = null;
+
+const readline = require('readline');
+const rl = readline.createInterface({input: process.stdin, output: process.stdout});
+
+rl.question('Enter git item name: ', ans => {
+    git_item_name = ans;
+
+
+    exec('git pull', (err, stdout, stderr) => {
+        console.log(err);
+    // handle err, stdout & stderr
+    });
+
+    // fs.writeFile('prod_hash.txt', git_item_name, function (err) {
+    //     if (err) return console.log(err);
+    //     console.log('Hello World > helloworld.txt');
+    // });
+
+
+
+
+    rl.close();
+});
+
+
