938843c252253d9994a914598344a8b77a780348
larrybozzly <larrybozzly@hotmail.com>
2022-09-15 00:06:05 +0300

    IN 127

--- test.js
+++ test.js
@@ -2,8 +2,6 @@ fs = require('fs');
 const { execSync } = require("child_process");
 
 
-
-
 let git_item_name = null;
 
 const readline = require('readline');
@@ -37,17 +35,35 @@ rl.question('Enter git item name: ', ans => {
         
         let string = `${latest_hash} !!! ${git_item_name}`.replace(/\n/g, ""); 
         string = '\n'+'\n'+string;
-
-        fs.readFile('log.txt', function (err, data) {
+        fs.readFile('log.txt', "utf8", function (err, data) {
             if (err) throw err;
             if(data.indexOf(git_item_name) >= 0){
+        
+                var reg = new RegExp('\\w*' + git_item_name + '\\w*', 'gi');
+                const hasArray = data.match(reg);
+                let lastElement = hasArray[hasArray.length - 1];
+        
+                let number;
+                if(lastElement.indexOf('_v') >= 0) {
+                    number = +lastElement.split("_v").pop();
+                    number = ++number;
+                } else {
+                    number = 2;
+                }
+        
                 let logger = fs.createWriteStream('log.txt', {
                     flags: 'a' // 'a' means appending (old data will be preserved)    
                 })
-                logger.write(string+'v1');
+                logger.write(`${string}_v${number}`);
+                logger.end();
+            } else {
+                let logger = fs.createWriteStream('log.txt', {
+                    flags: 'a' // 'a' means appending (old data will be preserved)    
+                })
+                logger.write(`${string}`);
                 logger.end();
             }
-          });
+        });
     }
     
     rl.close();
