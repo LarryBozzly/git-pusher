17f5d5115b721d8098cbd151719119a34e124bf6
larrybozzly <larrybozzly@hotmail.com>
2022-09-14 19:11:15 +0300

    testmehere

--- test.js
+++ test.js
@@ -6,52 +6,47 @@ const { execSync } = require("child_process");
 
 let git_item_name = null;
 
-function myFunction1() {
-    return execSync('git add .', (err, stdout, stderr) => {
-        // handle err, stdout & stderr
-    });
-}
-function myFunction2() {
-    return execSync(`git commit -a -m "aaaaaa"`, (err, stdout, stderr) => {
-        // handle err, stdout & stderr
-    });
-}
+const readline = require('readline');
+const rl = readline.createInterface({input: process.stdin, output: process.stdout});
 
-function myFunction3() {
-    return execSync(`git push`, (err, stdout, stderr) => {
-        // handle err, stdout & stderr
-    });
-}
-  
+rl.question('Enter git item name: ', ans => {
+    git_item_name = ans;
 
-/////test1/////
-myFunction1();
-myFunction2();
-myFunction3();
-
-  
-  
-
-
-// const readline = require('readline');
-// const rl = readline.createInterface({input: process.stdin, output: process.stdout});
-
-// rl.question('Enter git item name: ', ans => {
-//     git_item_name = ans;
-
+    function myFunction1() {
+        return execSync('git add .', (err, stdout, stderr) => {
+            // handle err, stdout & stderr
+        });
+    }
+    function myFunction2() {
+        return execSync(`git commit -a -m "${git_item_name}"`, (err, stdout, stderr) => {
+            // handle err, stdout & stderr
+        });
+    }
+    
+    function myFunction3() {
+        return execSync(`git push`, (err, stdout, stderr) => {
+            // handle err, stdout & stderr
+        });
+    }
+      
+    
+    /////test1/////
+    myFunction1();
+    myFunction2();
+    myFunction3();
     
 
     
     
-//     // fs.writeFile('prod_hash.txt', git_item_name, function (err) {
-//     //     if (err) return console.log(err);
-//     //     console.log('Hello World > helloworld.txt');
-//     // });
+    // fs.writeFile('prod_hash.txt', git_item_name, function (err) {
+    //     if (err) return console.log(err);
+    //     console.log('Hello World > helloworld.txt');
+    // });
 
 
 
 
-//     rl.close();
-// });
+    rl.close();
+});
 
 
